657c232be963ee813c8eda09db8bbf0c04c3fa71
NicoleNotUnix <nicolenotunix@fb.com>
2019-12-09 16:20:37 +0000

    Changing number of plays before ads for dev purposes

--- 4x4/InterstitialPresenter/InterstitialPresenter.m
+++ 4x4/InterstitialPresenter/InterstitialPresenter.m
@@ -11,12 +11,32 @@
 #import "AdVideoViewController.h"
 #import "AdViewController.h"
 
-@implementation InterstitialPresenter
+@implementation InterstitialPresenter {
+  int _currentTipCount;
+}
+
+- (instancetype)init {
+  if (self = [super init]) {
+    _currentTipCount = 0;
+  }
+  return self;
+}
 
 - (void)showInterstitialFromViewController:(UIViewController *)controller
 {
   UIViewController *nextVC = [AdViewController new]; // TODO: Randomize which one is shown, make sure the same one isn't shown multiple times
-  [controller showViewController:nextVC sender:self];
+  [controller showViewController:nextVC sender:controller];
+  _currentTipCount++;
+}
+
+- (UIViewController *)createVideoTipVC
+{
+  return [AdVideoViewController new];
+}
+
+- (UIViewController *)createQuestionTipVC
+{
+  return [AdViewController new];
 }
 
 @end
